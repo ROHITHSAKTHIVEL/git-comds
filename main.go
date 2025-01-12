57db605f65d1835b9af5f0ea4c91a60f46d1980d
ROHITH <rohiths.ee20@bitsathy.ac.in>
2025-01-13 00:46:56 +0530

    make rebase

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("make conflit while m2 merge")
-	fmt.Println("first commit conflict makes in main")
+	fmt.Println("first commit conflict makes in main to check rebase works how")
 }
